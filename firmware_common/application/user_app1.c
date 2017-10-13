228702581a09176301788d97b4a53c9f846d030e
AaronFarquharson <32115845+AaronFarquharson@users.noreply.github.com>
2017-10-13 08:51:32 -0600

    2017/10/13-3

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -176,17 +176,36 @@ static void UserApp1SM_Idle(void)
     }
     else
       UserApp1_u32Counter++;*/
-    if(UserApp1_u32counter == (500*UserApp1_u8evencount)
+    if(UserApp1_u32Counter <= (((1000/UserApp1_blink_rate)*UserApp1_u8evencount)+UserApp1_u32sum) && UserApp1_u32Counter >= (((1000/UserApp1_blink_rate)*UserApp1_u8oddcount)+UserApp1_u32sum))
     {
       HEARTBEAT_ON();
-      UserApp1_u8evencount += 2;
     }
-    if(UserApp1_u32counter == (500*UserApp1_u8oddcount)
+    if(UserApp1_u32Counter <= (((1000/UserApp1_blink_rate)*UserApp1_u8oddcount)+UserApp1_u32sum) && UserApp1_u32Counter <= (((1000/UserApp1_blink_rate)*UserApp1_u8evencount)+UserApp1_u32sum))
     {
       HEARTBEAT_OFF();
-      UserApp1_u8oddcount += 2;
     }
-    UserApp1_u32counter++;
+    if(UserApp1_u32Counter >= (((1000/UserApp1_blink_rate)*UserApp1_u8evencount)+UserApp1_u32sum))
+    {
+      UserApp1_u8evencount += 2;
+      UserApp1_u32sum = UserApp1_u32Counter;
+    }
+    if(UserApp1_u32Counter >= (((1000/UserApp1_blink_rate)*UserApp1_u8oddcount)+UserApp1_u32sum))
+    {
+      UserApp1_u8oddcount += 2;
+      UserApp1_u32sum = UserApp1_u32Counter;
+    }
+    if(UserApp1_u32Counter == (UserApp1_u8solid * 2000) && UserApp1_u32Counter != 0)
+    {
+      if(UserApp1_increment == 1)
+        UserApp1_blink_rate *= 6;
+      else
+        UserApp1_blink_rate /= 6;
+      
+      if(UserApp1_u8solid == 10 || UserApp1_u8solid == 0)
+        UserApp1_increment *= -1;
+      UserApp1_u8solid += UserApp1_increment;
+    }
+    UserApp1_u32Counter++;
     /*End of Assignment #2 code*/
 } /* end UserApp1SM_Idle() */
     
